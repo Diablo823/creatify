37b8d54d25db10e1c114fa009feb928166c6071f
KASI VISWANATHAN <kesukesu718@gmail.com>
2024-11-03 14:57:13 +0530

    second commit

--- components/shared/Mobilenav.tsx
+++ components/shared/Mobilenav.tsx
@@ -7,9 +7,6 @@ import React from "react";
 import {
   Sheet,
   SheetContent,
-  SheetDescription,
-  SheetHeader,
-  SheetTitle,
   SheetTrigger,
 } from "../ui/sheet";
 import { navLinks } from "@/constants";
